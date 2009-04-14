bf5ec96ea28621ab49c02a277b8a521726554fc4
Adrian Dmitra <adrian@dmitra.com>
2009-04-14 18:44:14 +0000

    Harter-Heighway dragon
    git-svn-id: http://newyork:81/svn/1/study/Ruby/fractal@26 3f256093-612f-4f29-bd12-348de49c2d45

--- Fractal.rb
+++ Fractal.rb
@@ -11,22 +11,26 @@ class Fractal
       @last = @shape
   end
   def self.sample
-     @@fractals = [[],["tick", "snowflake", "tree_cutout", "pyramid", "branched_tree"],[15,5,6,15,12]]
+     @@samples = [[],["tick", "snowflake", "tree_cutout", "tree_1", "dragon", "pyramid", "branched_tree", "rounded_tree"],[15,5,6,6,16,15,12,0]]
      line1 = Line.rel(400,750,700,0)
-     @@fractals[0] << Fractal.new(line1, @@fractals[1][0])
+     @@samples[0] << Fractal.new(line1, @@samples[1][0])
      triangle = Triangle.new(Line.rel(700,500, -200, 0),
        Line.rel(500,500,100,-(3**0.5)*100),
        Line.rel(600,500-(3**0.5)*100, 100,100*(3**0.5)))
      triangle.zoom(5, triangle.midst)
-     @@fractals[0] << Fractal.new(triangle, @@fractals[1][1])
-     
+     @@samples[0] << Fractal.new(triangle, @@samples[1][1])
      line2 = Line.rel(800,1000,0,-800)
-     @@fractals[0] << Fractal.new(line2, @@fractals[1][2])
+     @@samples[0] << Fractal.new(line2, @@samples[1][2])
+     @@samples[0] << Fractal.new(line2, @@samples[1][3])
+     line6 = Line.rel(1000,800, -600, 0)
+     @@samples[0] << Fractal.new(line6, @@samples[1][4])
      line3 = Line.rel(700,500,50,0)
-     @@fractals[0] << Fractal.new(line3, @@fractals[1][3])
-     line4= Line.rel(600,900,0,-100)
-     @@fractals[0] << Fractal.new(line4, @@fractals[1][4])
-     return @@fractals
+     @@samples[0] << Fractal.new(line3, @@samples[1][5])
+     line4 = Line.rel(600,900,0,-100)
+     @@samples[0] << Fractal.new(line4, @@samples[1][6])
+     line5 = Line.rel(700,900,0,-1)
+     @@samples[0] << Fractal.new(line5, @@samples[1][7])
+     return @@samples
   end
 #Iterating function changes itSELF because it is a Fractal - "changing geometry of figure"
   def iter(iterations, continue)
@@ -46,7 +50,7 @@ class Fractal
         @shape.shift                                 #удаление раздробленной линии
         } #end of ticking
       } #end of iterations
-    when "snowflake"
+    when "snowflake" #на основе триадной кривой Коха
       iterations.times{
         @shape.each{|line|
          arr = line.divide(3)
@@ -54,27 +58,38 @@ class Fractal
          result.shift
         }; @shape = result
       }
+    when "tree_1"
+      iterations.times{
+        @shape.each{|line|
+          arr = line.divide(2)
+          @shape += [arr[0]] + arr[1].tick("clockwise", 45) + arr[1].tick("counterclockwise", 45)
+          @shape.shift
+        }
+     }
     when "tree_cutout"
-      half1 = @shape
-      half2 = @shape
+      @shape = @shape*2
       iterations.times{
-        half1.each{|line|
-          arr = line.divide(2)
-          result += [arr[0]] + arr[1].tick("clockwise", 45)
-          result.shift
-        }
-        half1 = result
-      }
-      result = @shape #refresh result with initial value
+          length = @shape.length
+          for i in 0 ... length
+            arr = @shape.shift.divide(2)
+            if i < length/2
+                @shape += [arr[0]] + arr[1].tick("clockwise", 45)
+            else
+                @shape += [arr[0]] + arr[1].tick("counterclockwise", 45)
+            end
+          end
+       }
+    when "dragon" #дракон Хартера-Хейтуэя
       iterations.times{
-        half2.each{|line|
-          arr = line.divide(2)
-          result += [arr[0]] + arr[1].tick("counterclockwise")
-          result.shift
-        }
-        half2 = result
-      }
-      @shape = half1 + half2
+        @shape.each_with_index{|line, index|
+        if index%2 == 0
+          @shape += line.tick("clockwise", 45)
+         else  
+          @shape += line.tick("counterclockwise", 45)
+        end
+        @shape.shift                                
+        } #end of ticking
+      } #end of iterations      
     when "branched_tree"
       if continue == true
         i = @last
@@ -90,6 +105,14 @@ class Fractal
         @last = i = result
         @shape += result
       }
+    when "rounded_tree"
+         chit = @shape.dup #oтросток
+         iterations.times{
+          chit.each{|line|
+              @shape << line.grow(rand*2-0.9, line.vector)
+              chit = [@shape.last]
+          }
+         }
     end
     return @shape
   end
